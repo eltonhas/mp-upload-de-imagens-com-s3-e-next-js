6beaf87524fe08737f65f711beeaa40ed267e212
eltonhas <elton_has@hotmail.com>
2024-08-27 16:48:59 -0300

    change name AWS_ACCESS_KEY credntials

--- src/lib/s3-client.ts
+++ src/lib/s3-client.ts
@@ -3,7 +3,7 @@ import { S3Client } from '@aws-sdk/client-s3'
 const s3Client = new S3Client({
   region: 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    accessKeyId: process.env.AWS_CLIENT_ACCESS_KEY as string,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 })
