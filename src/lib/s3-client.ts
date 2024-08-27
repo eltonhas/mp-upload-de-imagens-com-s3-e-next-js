6fd2132703c2ab153f8df57a8e77a0f0e1f3a9b1
eltonhas <elton_has@hotmail.com>
2024-08-27 16:43:30 -0300

    put credntials

--- src/lib/s3-client.ts
+++ src/lib/s3-client.ts
@@ -3,10 +3,8 @@ import { S3Client } from '@aws-sdk/client-s3'
 const s3Client = new S3Client({
   region: 'us-east-1',
   credentials: {
-    // accessKeyId: process.env.AWS_ACCESS_KEY as string,
-    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-    accessKeyId: '',
-    secretAccessKey: '',
+    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 })
 
